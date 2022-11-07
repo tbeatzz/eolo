6c1fe9dcbcb04061fa4e4c9f5083f74b376afb98
tbeatzz <tbeatzz20k@gmail.com>
2022-11-07 14:56:30 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const menuBtn = document.querySelector(".header__burger");
+let nav = document.querySelector(".header__ul");
+menuBtn.addEventListener("click", function () {
+  nav.classList.toggle("header__ul--activo");
+});
